67e226ed62c63f5de1b39d139fc984a64976d3a4
bryancrisso <bryancrisso@outlook.com>
2024-05-09 16:03:16 +0100

    Create calculation function for sunrise time.

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import { sunrise } from "./calculations";
 
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-orange-500">
-            <Text>Open up App.js to start working on your app!</Text>
+            <Text>{sunrise(130, 15, 52, 25, 0, 0.141600, 52.196280)}</Text>
             <StatusBar style="auto" />
         </View>
     );
