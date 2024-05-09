14b8ca06ef9aad7da0d5b24514e9e21517afcc12
Sophie Chiang <sophieyoshi2014@gmail.com>
2024-05-09 15:28:55 +0100

    changes

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import Cat from "./components/Api"
 
 export default function App() {
     return (
         <View className="flex-1 items-center justify-center bg-orange-500">
             <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
+            <Cat />
         </View>
     );
 }
