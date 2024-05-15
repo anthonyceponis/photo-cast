debf647d5fb18347ef2fcd8b0f20ed36300445d1
Sophie Chiang <sophieyoshi2014@gmail.com>
2024-05-15 21:23:05 +0100

    new screen templates in the side menu bar

---
+++ screens/Tips.js
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export function TipsScreen() {
+    return (
+        <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Text>This is the Tips Screen </Text>
+        </View>
+    );
+};
