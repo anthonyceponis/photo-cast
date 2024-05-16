2428c9ad6ee6ac9c4158d27d961f9e57258c0662
anthonyceponis <anthonyceponis@gmail.com>
2024-05-16 16:06:44 +0100

    Implement search by condition in weather box

--- App.tsx
+++ App.tsx
@@ -1,18 +1,30 @@
-import { View } from "react-native";
+import { Pressable, SafeAreaView, View } from "react-native";
 import { Nav } from "./components/nav";
 import { Footer } from "./components/footer";
 import { Map } from "./components/map";
 import { SideNav } from "./components/sidenav";
 import { WeatherContainer } from "./components/weather-box/container";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 export default function App() {
+    const [weatherBoxOpen, setWeatherBoxOpen] = useState<boolean>(false);
     return (
-        <View className="flex-1 items-center justify-center bg-white">
+        <SafeAreaView className="flex-1 items-center justify-center bg-white">
             <Nav />
-            <SideNav />
-            <Map />
-            <WeatherContainer />
+            {/* <SideNav /> */}
+            {/* <Map /> */}
+            <Pressable onPress={() => setWeatherBoxOpen(true)}>
+                <FontAwesomeIcon icon={faSearch} />
+            </Pressable>
+            {weatherBoxOpen ? (
+                <WeatherContainer
+                    isOpen={weatherBoxOpen}
+                    setIsOpen={setWeatherBoxOpen}
+                />
+            ) : null}
             <Footer />
-        </View>
+        </SafeAreaView>
     );
 }
