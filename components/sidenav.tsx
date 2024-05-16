a8745037e65e7143526e5e14de321fa25d6293e7
Sophie Chiang <sophieyoshi2014@gmail.com>
2024-05-16 08:51:49 +0100

    sidenav component from homescreen

--- components/sidenav.tsx
+++ components/sidenav.tsx
@@ -1,9 +1,18 @@
-import { Text, View } from "react-native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { TipsScreen } from "../screens/Tips";
+import { SettingsScreen } from "../screens/Settings";
+import { FavouritesScreen } from "../screens/Favourites";
+import { HomeScreen } from "../App";
 
-export const SideNav = () => {
+const Drawer = createDrawerNavigator();
+
+export function SideNav() {
     return (
-        <View>
-            <Text>This is the sidenav.</Text>
-        </View>
+        <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={HomeScreen} />
+            <Drawer.Screen name="Tips" component={TipsScreen} />
+            <Drawer.Screen name="Settings" component={SettingsScreen} />
+            <Drawer.Screen name="Favourites" component={FavouritesScreen} />
+        </Drawer.Navigator>
     );
-};
+}
